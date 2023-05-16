fd690a1785bbe23d38413ed29f0d5407f4795207
Dmitry "ts.kdv.raz0r" Kolganov <ts.kdv.raz0r@ya.ru>
2023-05-17 00:35:02 +0300

    Тесты для урока 2.2.

---
+++ Tests/TestModuleTwo/test_lesson_two.py
@@ -0,0 +1,51 @@
+import unittest
+
+from Source.ModuleTwo.LessonTwo import hello_world
+from Source.ModuleTwo.LessonTwo import lucky_sequence_1
+from Source.ModuleTwo.LessonTwo import star_triangle
+from Source.ModuleTwo.LessonTwo import greetings
+from Source.ModuleTwo.LessonTwo import favorite_team
+from Source.ModuleTwo.LessonTwo import repeat_after_me
+from Source.ModuleTwo.LessonTwo import repeat_after_me_two
+
+
+class TestLessonTwo(unittest.TestCase):
+    @staticmethod
+    def test_hello_world() -> None:
+        assert hello_world() == "Здравствуй, мир!"
+
+    @staticmethod
+    def test_lucky_sequence_1() -> None:
+        assert lucky_sequence_1() == (4, 8, 15, 16, 23, 42)
+
+    @staticmethod
+    def test_star_triangle() -> None:
+        assert star_triangle() == ['*', '**', '***', '****', '*****', '******', '*******']
+
+    @staticmethod
+    def test_greetings() -> None:
+        assert greetings("Тимур") == "Привет, Тимур"
+        assert greetings("Гвидо") == "Привет, Гвидо"
+        assert greetings("Питоняшка") == "Привет, Питоняшка"
+
+    @staticmethod
+    def test_favorite_team() -> None:
+        assert favorite_team("ЦСКА") == "ЦСКА - чемпион!"
+        assert favorite_team("Локомотив") == "Локомотив - чемпион!"
+        assert favorite_team("Зенит") == "Зенит - чемпион!"
+
+    @staticmethod
+    def test_repeat_after_me() -> None:
+        assert repeat_after_me(["I love", "Python", "so much"]) == ["I love", "Python", "so much"]
+        assert repeat_after_me(["I used", "sys.stdin", "for this func"]) == ["I used", "sys.stdin", "for this func"]
+        assert repeat_after_me(["I live", "in", "St.Petersburg"]) == ["I live", "in", "St.Petersburg"]
+
+    @staticmethod
+    def test_repeat_after_me_two() -> None:
+        assert repeat_after_me_two(["I love", "Python", "so much"]) == ["so much", "Python", "I love"]
+        assert repeat_after_me_two(["I used", "sys.stdin", "for this func"]) == ["for this func", "sys.stdin", "I used"]
+        assert repeat_after_me_two(["I live", "in", "St.Petersburg"]) == ["St.Petersburg", "in", "I live"]
+
+
+if __name__ == '__main__':
+    unittest.main()
